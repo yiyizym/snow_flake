54e71067cfe6207027ec0280c06afb5b36d90744
jude <jude.zhu@whalemove.com>
2017-03-02 10:20:04 +0800

    分离 id/key && fixbug

---
+++ js/idkey.js
@@ -0,0 +1,7 @@
+//TODO 上传到 github 前清空 APP_ID APP_KEY ！！！
+// 应用 ID，用来识别应用
+window.APP_ID = '';
+
+// 应用 Key，用来校验权限（Web 端可以配置安全域名来保护数据安全）
+window.APP_KEY = '';
+//TODO 上传到 github 前清空 APP_ID APP_KEY ！！！
