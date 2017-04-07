341c3e92d4f1c187bea50f866d0a7f7daf261357
jude <jude.zhu@whalemove.com>
2017-04-07 12:09:29 +0800

    增加国际化

---
+++ config/locale.js
@@ -0,0 +1,43 @@
+/**
+ * Created by zym on 17年4月7日.
+ */
+window.LANGUAGE = {
+  'zh': {
+    'signUp': '注册',
+    'signIn': '登录',
+    'logOut': '登出',
+    'greetings': '你好，',
+    'optionalLanguage': 'english',
+    'email': '邮箱',
+    'pwd': '密码',
+    'confirm': '确定',
+    'cancel': '取消',
+    'steps': '步骤',
+    'saving': '正在保存',
+    'saveAll': '保存所有',
+    'validateEmailMsg': '请验证邮箱，验证成功后点击“我已验证邮箱，登录”按钮',
+    'validatedLogInMsg': '我已验证邮箱，登录',
+    'sessionTimeout': '会话过期，请先登录！',
+    'saved': '保存成功',
+    'savedFailed': '保存失败',
+  },
+  'en': {
+    'signUp': 'sign up',
+    'signIn': 'sign in',
+    'logOut': 'log out',
+    'greetings': 'Hello, ',
+    'optionalLanguage': '中文',
+    'email': 'Email',
+    'pwd': 'Password',
+    'confirm': 'confirm',
+    'cancel': 'cancel',
+    'steps': 'steps',
+    'saving': 'saving',
+    'saveAll': 'save all',
+    'validateEmailMsg': 'After verified your email, click the following button',
+    'validatedLogInMsg': 'Log me in',
+    'sessionTimeout': 'session timeout, please log in',
+    'saved': 'saved',
+    'savedFailed': 'saved failed, please try again',
+  }
+};
